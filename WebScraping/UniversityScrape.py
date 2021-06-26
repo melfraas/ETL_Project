cd3c4d1c66ce045a7763183bbfaa80355f24a853
matteo005 <47016468+matteo005@users.noreply.github.com>
2021-06-26 12:32:11 -0400

    adding the .py file with the current data pulled

--- WebScraping/UniversityScrape.py
+++ WebScraping/UniversityScrape.py
@@ -3,6 +3,11 @@ import requests
 
 html_text = requests.get('https://university.graduateshotline.com/ubystate.html').text
 soup = BeautifulSoup(html_text, 'lxml')
+numofstates = soup.find_all('b')
+universities = soup.find_all('a')
+
+for state in range(1,len(numofstates)):
+    print(numofstates[state].text)
+    
 
 
-print(html_text)
