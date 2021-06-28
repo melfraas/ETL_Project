381c2af58e6c990789659a356ad378b991e6dc4f
matteo005 <47016468+matteo005@users.noreply.github.com>
2021-06-28 18:46:38 -0400

    added the jupiter notebook with the Pandas code along with the generated CSV file.

--- WebScraping/UniversityScrape.py
+++ WebScraping/UniversityScrape.py
@@ -1,28 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-html_text = requests.get('https://university.graduateshotline.com/ubystate.html').text
+html_text = requests.get('https://www.university-list.net/us/universities-1000.htm').text
 soup = BeautifulSoup(html_text, 'lxml')
-numofstates = soup.find_all('b')
-universities = soup.find_all('a')
-ols = soup.find_all('ol')
+universitysite = []
+university = []
 
+for a in soup.find_all('a', href=True): 
+    if a.text: 
+        universitysite.append(a.href)
 
-allstates = []
-alluniversities = []
+for a in soup.find_all('a', href=True): 
+    if a.text: 
+        university.append(a.text)
 
-for state in numofstates:
-    allstates.append(state.text)   
-
-for ol in ols:
-    lis = ol.find_all('li')
-    alllis = []
-    for li in lis:
-        alllis.append(li.text)
-    alluniversities.append(alllis)
-    
-print(alluniversities)
-        
+print(university)
     
 
 
